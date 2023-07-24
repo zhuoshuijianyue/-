bd07c08124a958585a034db38565358bf963ab8c
Guo Wangchang <guowch3@mail2.sysu.edu.cn>
2023-07-24 12:24:29 +0800

    fix bug in verilog practice

--- npc/lab7/vsrc/top.v
+++ npc/lab7/vsrc/top.v
@@ -3,11 +3,13 @@ module top (
     output[7:0] seg0,
     output[7:0] seg1,
     output[7:0] seg2,
-    output[7:0] seg3
+    output[7:0] seg3,
+    output[7:0] seg4,
+    output[7:0] seg5
 );
     
-    wire[7:0] seg_key,key_count;
-    wire[7:0] seg0_not,seg1_not,seg2_not,seg3_not;
+    wire[7:0] seg_key,key_count,asc2i_temp;
+    wire[7:0] seg0_not,seg1_not,seg2_not,seg3_not,seg4_not,seg5_not;
     reg[7:0] seg_key_temp;
     wire state;
     ps2_keyboard my_keyboard(
@@ -21,9 +23,15 @@ module top (
 );
    
   MuxKey #(2, 1, 8) s0_mux (seg_key_temp, state,{
-    1'b0,seg_key,
-    1'b1,8'd0
+    1'b1,seg_key,
+    1'b0,8'd0
   });
+
+  key2asc2i u_key2asc2i(
+    .seg_key_temp ( seg_key_temp ),
+    .asc2i_temp  ( asc2i_temp  )
+  );
+
 
    MuxKey #(16, 4, 8) u0_mux (seg0_not, seg_key_temp[3:0],{
     4'd0, 8'b00000000,
@@ -61,7 +69,7 @@ module top (
     4'd14,8'b10011110,
     4'd15,8'b10001110
   });
-  MuxKey #(16, 4, 8) u2_mux (seg2_not, key_count[3:0], {
+  MuxKey #(16, 4, 8) u2_mux (seg2_not, asc2i_temp[3:0], {
     4'd0, 8'b11111100,
     4'd1, 8'b01100000,
     4'd2, 8'b11011010,
@@ -79,7 +87,7 @@ module top (
     4'd14,8'b10011110,
     4'd15,8'b10001110
   });
-  MuxKey #(16, 4, 8) u3_mux (seg3_not, key_count[7:4], {
+  MuxKey #(16, 4, 8) u3_mux (seg3_not, asc2i_temp[7:4], {
     4'd0, 8'b11111100,
     4'd1, 8'b01100000,
     4'd2, 8'b11011010,
@@ -97,10 +105,48 @@ module top (
     4'd14,8'b10011110,
     4'd15,8'b10001110
   });
-  assign seg0=~seg0_not;
-  assign seg1=~seg1_not;
-  assign seg2=~seg2_not;
-  assign seg3=~seg3_not;
+  MuxKey #(16, 4, 8) u4_mux (seg4_not, key_count[3:0], {
+    4'd0, 8'b11111100,
+    4'd1, 8'b01100000,
+    4'd2, 8'b11011010,
+    4'd3, 8'b11110010,
+    4'd4, 8'b01100110,
+    4'd5, 8'b10110110,
+    4'd6, 8'b10111110,
+    4'd7, 8'b11100000,
+    4'd8, 8'b11111110,
+    4'd9, 8'b11110110,
+    4'd10,8'b11101110,
+    4'd11,8'b00111110,
+    4'd12,8'b10011100,
+    4'd13,8'b01111010,
+    4'd14,8'b10011110,
+    4'd15,8'b10001110
+  });
+  MuxKey #(16, 4, 8) u5_mux (seg5_not, key_count[7:4], {
+    4'd0, 8'b11111100,
+    4'd1, 8'b01100000,
+    4'd2, 8'b11011010,
+    4'd3, 8'b11110010,
+    4'd4, 8'b01100110,
+    4'd5, 8'b10110110,
+    4'd6, 8'b10111110,
+    4'd7, 8'b11100000,
+    4'd8, 8'b11111110,
+    4'd9, 8'b11110110,
+    4'd10,8'b11101110,
+    4'd11,8'b00111110,
+    4'd12,8'b10011100,
+    4'd13,8'b01111010,
+    4'd14,8'b10011110,
+    4'd15,8'b10001110
+  });
+  assign seg0=state?~seg0_not:8'b11111111;
+  assign seg1=state?~seg1_not:8'b11111111;
+  assign seg2=state?~seg2_not:8'b11111111;
+  assign seg3=state?~seg3_not:8'b11111111;
+  assign seg4=~seg4_not;
+  assign seg5=~seg5_not;
 endmodule
 
 module ps2_keyboard(clk,resetn,ps2_clk,ps2_data,seg_key,key_count,state);
@@ -141,12 +187,12 @@ module ps2_keyboard(clk,resetn,ps2_clk,ps2_data,seg_key,key_count,state);
                     if(seg_key==buffer[8:1]&&seg_key_temp==8'hf0) 
                     begin
                       key_count<=key_count+1;
-                      state<=0;
+                      state<=1;
                     end
                     else if(seg_key!=buffer[8:1]&&seg_key!=8'hf0&&buffer[8:1]!=8'hf0) 
                     begin
                       key_count<=key_count+1;
-                      state<=0;
+                      state<=1;
                     end
                     else if(seg_key==buffer[8:1]&&seg_key_temp!=8'hf0)
                     begin
@@ -154,7 +200,7 @@ module ps2_keyboard(clk,resetn,ps2_clk,ps2_data,seg_key,key_count,state);
                     end
                     else if(seg_key!=buffer[8:1]&&(seg_key==8'hf0||buffer[8:1]==8'hf0))
                     begin
-                      state<=1;
+                      state<=0;
                     end
                 end
                 count <= 0;     // for next
@@ -165,4 +211,49 @@ module ps2_keyboard(clk,resetn,ps2_clk,ps2_data,seg_key,key_count,state);
             end
         end
     end
+endmodule
+
+module key2asc2i(
+  input[7:0] seg_key_temp,
+  output[7:0] asc2i_temp
+  );
+  //MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 1) i0 (out, key, default_out, lut);
+  MuxKeyInternal #(36, 8, 8,1) u0_mux (asc2i_temp, seg_key_temp,8'd0,{
+    8'h70, 8'h30,
+    8'h69, 8'h31,
+    8'h72, 8'h32,
+    8'h7A, 8'h33,
+    8'h6B, 8'h34,
+    8'h73, 8'h35,
+    8'h74, 8'h36,
+    8'h6c, 8'h37,
+    8'h75, 8'h38,
+    8'h7D, 8'h39,
+    8'h1c, 8'h61,
+    8'h32, 8'h62,
+    8'h21, 8'h63,
+    8'h23, 8'h64,
+    8'h24, 8'h65,
+    8'h2b, 8'h66,
+    8'h34, 8'h67,
+    8'h33, 8'h68,
+    8'h43, 8'h69,
+    8'h3b, 8'h70,
+    8'h42, 8'h71,
+    8'h4b, 8'h72,
+    8'h3a, 8'h73,
+    8'h31, 8'h74,
+    8'h44, 8'h75,
+    8'h4d, 8'h76,
+    8'h15, 8'h77,
+    8'h2d, 8'h78,
+    8'h1b, 8'h79,
+    8'h2c, 8'h80,
+    8'h3c, 8'h81,
+    8'h2a, 8'h82,
+    8'h1d, 8'h83,
+    8'h22, 8'h84,
+    8'h35, 8'h85,
+    8'h1a, 8'h86
+  });
 endmodule
